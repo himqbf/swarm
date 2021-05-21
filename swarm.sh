68d54e2bfa91ebc6adf8ca6a08ff73be506f0b17
himqbf <361552872@qq.com>
2021-05-21 23:33:14 +0800

    ds

--- swarm.sh
+++ swarm.sh
@@ -6,6 +6,10 @@ wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_
 rpm -i bee-clef_0.4.12_amd64.rpm
 wget https://github.com/ethersphere/bee/releases/download/v0.6.0/bee_0.6.0_amd64.rpm
 rpm -i bee_0.6.0_amd64.rpm
+wget https://raw.githubusercontent.com/himqbf/swarm/main/clefstart.sh
+wget https://raw.githubusercontent.com/himqbf/swarm/main/beestart.sh
+chmod +x clefstart.sh
+chmod +x beestart.sh
 yum install -y expect
 yum install -y screen
 clefcmd="bash clefstart.sh"
