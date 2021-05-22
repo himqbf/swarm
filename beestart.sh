4f00176fdac4813537f363d001a7084811a5c36b
himqbf <361552872@qq.com>
2021-05-22 15:00:02 +0800

    k

--- beestart.sh
+++ beestart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 spawn bee start --verbosity 5 --swap-endpoint https://goerli.infura.io/v3/5d419ee9ec5c42a79adf1bce37efaa97 --debug-api-enable --clef-signer-enable --clef-signer-endpoint /var/lib/bee-clef/clef.ipc
-expect "*Password*"
+expect "*Password"
 send "qbfqq19920111\n"
-expect "*Confirm password*"
+expect "*Confirm*"
 send "qbfqq19920111\n"
