fed8951007a0642e71cc673c67bccc6f604c8c19
himqbf <361552872@qq.com>
2021-05-22 17:32:34 +0800

    s

--- swarm.sh
+++ swarm.sh
@@ -3,7 +3,7 @@ mkdir bee
 cd bee
 yum install -y wget
 wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.10/bee-clef_0.4.10_amd64.rpm
-rpm -i bee-clef_0.4.12_amd64.rpm
+rpm -i bee-clef_0.4.10_amd64.rpm
 wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.rpm
 rpm -i bee_0.5.3_amd64.rpm
 wget https://raw.githubusercontent.com/himqbf/swarm/main/clefstart.sh
