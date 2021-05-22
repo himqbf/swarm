2f0d54055b1b2e4ef123517f157d9059335051e8
himqbf <361552872@qq.com>
2021-05-22 14:44:35 +0800

    d

--- swarm.sh
+++ swarm.sh
@@ -4,8 +4,8 @@ cd bee
 yum install -y wget
 wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_0.4.12_amd64.rpm
 rpm -i bee-clef_0.4.12_amd64.rpm
-wget https://github.com/ethersphere/bee/releases/download/v0.6.0/bee_0.6.0_amd64.rpm
-rpm -i bee_0.6.0_amd64.rpm
+wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.rpm
+rpm -i bee_0.5.3_amd64.rpm
 wget https://raw.githubusercontent.com/himqbf/swarm/main/clefstart.sh
 wget https://raw.githubusercontent.com/himqbf/swarm/main/beestart.sh
 chmod +x clefstart.sh
@@ -13,12 +13,14 @@ chmod +x beestart.sh
 yum install -y expect
 yum install -y screen
 screen -dmS clef
-clefcmd="bash clefstart.sh"
+clefcmd="./clefstart.sh"
 screen -x -S clef -p 0 -X stuff "$clefcmd"
+screen -x -S clef -p 0 -X stuff $'\n'
 
 screen -dmS bee
-beecmd="bash beestart.sh"
+beecmd="./beestart.sh"
 screen -x -S bee -p 0 -X stuff "$beecmd"
+screen -x -S bee -p 0 -X stuff $'\n'
 
 yum install -y epel-release
 yum list jq
